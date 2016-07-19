7308c3348b96bbcf76992e38fa2143075acbffb4
Kaylier <kaylier@openmailbox.org>
2016-07-19 22:35:00 +0200

    debug du prompt et modif de l'install

--- install.sh
+++ install.sh
@@ -18,34 +18,38 @@ then
     esac
 fi
 
-echo ${DOTFILES_DIR-}
-
 # Update dotfiles
 if [ -d "${DOTFILES_DIR}/.git" ]
 then 
-	echo git --work-tree="${DOTFILES_DIR}" --git-dir="${DOTFILES_DIR}/.git" pull origin master
+    echo "Pull repository..."
+	git --work-tree="${DOTFILES_DIR}" --git-dir="${DOTFILES_DIR}/.git" pull origin master
 else
 	echo "Missing git repository: ${DOTFILES_DIR}/.git"
     exit 1
 fi
 
-echo ln -sbv ${DOTFILES_DIR}/git/gitconfig       ${HOME}/.gitconfig
-echo ln -sbv ${DOTFILES_DIR}/i3/config           ${HOME}/.config/i3/config
-echo ln -sbv ${DOTFILES_DIR}/i3/conky-i3bar      ${HOME}/.config/i3/conky-i3bar
-echo ln -sbv ${DOTFILES_DIR}/i3/conky-i3bar.sh   ${HOME}/.config/i3/conky-i3bar.sh
-echo ln -sbv ${DOTFILES_DIR}/i3/workspace1.json  ${HOME}/.config/i3/workspace1
-echo ln -sbv ${DOTFILES_DIR}/i3/workspace3.json  ${HOME}/.config/i3/workspace3
-echo ln -sbv ${DOTFILES_DIR}/i3/workspace4.json  ${HOME}/.config/i3/workspace4
-echo ln -sbv ${DOTFILES_DIR}/urxvt/Xresources    ${HOME}/.Xresources
-echo ln -sbv ${DOTFILES_DIR}/vim/vimrc           /etc/vimrc
-echo ln -sbv ${DOTFILES_DIR}/zsh/zlogin          /etc/zsh/zlogin
-echo ln -sbv ${DOTFILES_DIR}/zsh/zlogout         /etc/zsh/zlogout
-echo ln -sbv ${DOTFILES_DIR}/zsh/zprofile        /etc/zsh/zprofile
-echo ln -sbv ${DOTFILES_DIR}/zsh/zshenv          /etc/zsh/zshenv
-echo ln -sbv ${DOTFILES_DIR}/zsh/zshrc           /etc/zsh/zshrc
-echo ln -sbv ${DOTFILES_DIR}/zsh/zshrc_aliases   /etc/zsh/zshrc_aliases
-echo ln -sbv ${DOTFILES_DIR}/zsh/zshrc_functions /etc/zsh/zshrc_functions
-echo ln -sbv ${DOTFILES_DIR}/zsh/zshrc_prompts   /etc/zsh/zshrc_prompts
+echo
+echo "Link configuration files..."
+ln -sfv ${DOTFILES_DIR}/git/gitconfig       ${HOME}/.gitconfig
+ln -sfv ${DOTFILES_DIR}/i3/config           ${HOME}/.config/i3/config
+ln -sfv ${DOTFILES_DIR}/i3/conky-i3bar      ${HOME}/.config/i3/conky-i3bar
+ln -sfv ${DOTFILES_DIR}/i3/conky-i3bar.sh   ${HOME}/.config/i3/conky-i3bar.sh
+ln -sfv ${DOTFILES_DIR}/i3/workspace1.json  ${HOME}/.config/i3/workspace1.json
+ln -sfv ${DOTFILES_DIR}/i3/workspace3.json  ${HOME}/.config/i3/workspace3.json
+ln -sfv ${DOTFILES_DIR}/i3/workspace4.json  ${HOME}/.config/i3/workspace4.json
+ln -sfv ${DOTFILES_DIR}/urxvt/Xresources    ${HOME}/.Xresources
+
+echo "Link system-wide configuration files..."
+
+sudo ln -sfv ${DOTFILES_DIR}/vim/vimrc           /etc/vimrc
+sudo ln -sfv ${DOTFILES_DIR}/zsh/zlogin          /etc/zsh/zlogin
+sudo ln -sfv ${DOTFILES_DIR}/zsh/zlogout         /etc/zsh/zlogout
+sudo ln -sfv ${DOTFILES_DIR}/zsh/zprofile        /etc/zsh/zprofile
+sudo ln -sfv ${DOTFILES_DIR}/zsh/zshenv          /etc/zsh/zshenv
+sudo ln -sfv ${DOTFILES_DIR}/zsh/zshrc           /etc/zsh/zshrc
+sudo ln -sfv ${DOTFILES_DIR}/zsh/zshrc_aliases   /etc/zsh/zshrc_aliases
+sudo ln -sfv ${DOTFILES_DIR}/zsh/zshrc_functions /etc/zsh/zshrc_functions
+sudo ln -sfv ${DOTFILES_DIR}/zsh/zshrc_prompts   /etc/zsh/zshrc_prompts
 
 
 exit 0
