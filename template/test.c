04a1dcab300d36dba80cb437398bbd01e5984b28
Kaylier <kaylier@openmailbox.org>
2017-10-03 11:02:13 -0400

    Add templates and minor modifications

---
+++ template/test.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <assert.h>
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
+
---
+++ template/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <random>
+#include <cmath>
+#include <vector>
+#include <cassert>
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
+
